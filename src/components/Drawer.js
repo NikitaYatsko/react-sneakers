ea23f1629753cdebdc65bd3ce4b879e6bf162213
Nikita <archon04@mail.ru>
2024-12-21 00:11:08 +0200

    html template for empty cart and show it if the cart is empty

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -13,7 +13,6 @@ const Drawer = ({onClose, onRemove, cartItems = []}) => {
     return (
         <div className='overlay'>
 
-
             <div className="drawer">
 
                 <h2 className="d-flex align-center justify-between ">
@@ -21,59 +20,65 @@ const Drawer = ({onClose, onRemove, cartItems = []}) => {
                     <img src="/img/cart-button.svg" alt="remove" className='cart-remove_btn cu-p' onClick={onClose}/>
                 </h2>
 
-                <div className='cart-empty'>
-                    <h3 className="text-center">Ваша корзина сейчас пуста :(</h3>
-                    <button className='order-button'>Вернуться назад</button>
-                </div>
+                {cartItems.length !== 0 ?
+                    <>
+                        <div className="items">
+                            {cartItems.map((obj, index) => (
+                                <div className="cart-item d-flex align-center justify-center " key={index}>
+                                    <div className='cart-item-img'>
+                                        <img src={obj.imageURL} alt="" className='cart-item-img'/>
+                                    </div>
+                                    <div className='ml-10'>
+                                        <p>
+                                            {obj.title}
+                                        </p>
+                                        <b>{obj.price} руб.</b>
+                                    </div>
+                                    <img onClick={() => onRemove(obj.id, calculateTotalPrice())} src="/img/cart-button.svg"
+                                         alt="remove" className='cart-remove_btn'
+                                    />
+                                </div>
+                            ))}
 
 
-                <div className="items">
-                    {cartItems.map((obj, index) => (
-                        <div className="cart-item d-flex align-center" key={index}>
-                            <div className='cart-item-img'>
-                                <img src={obj.imageURL} alt="" className='cart-item-img'/>
-                            </div>
-                            <div className='ml-10'>
-                                <p>
-                                    {obj.title}
-                                </p>
-                                <b>{obj.price} руб.</b>
-                            </div>
-                            <img onClick={() => onRemove(obj.id, calculateTotalPrice())} src="/img/cart-button.svg"
-                                 alt="remove" className='cart-remove_btn'
-                            />
                         </div>
-                    ))}
+                        <div className='cart-total_block'>
+                            <ul>
+                                <li>
+                                <span>
+                                    Итого
+                                </span>
+                                    <div>
 
+                                    </div>
+                                    <b>
+                                        {calculateTotalPrice()} руб.
+                                    </b>
+                                </li>
+                                <li>
+                                <span>
+                                    Налог 5%
+                                </span>
+                                    <div>
 
-                </div>
-                <div className='cart-total_block'>
-                    <ul>
-                        <li>
-                            <span>
-                                Итого
-                            </span>
-                            <div>
+                                    </div>
+                                    <b>
+                                        {tax.toFixed(1)} руб.
+                                    </b>
+                                </li>
+                            </ul>
+                            <button className='order-button'>Оформить заказ <img src="/img/arrow.svg" alt="arrow"/></button>
+                        </div>
+                </> : <div className='cart-empty'>
+                    <img width={120} height={120} src="/img/box.svg" alt="box-image"/>
+                    <h3 className="text-center h3">Корзина пустая</h3>
 
-                            </div>
-                            <b>
-                                {calculateTotalPrice()} руб.
-                            </b>
-                        </li>
-                        <li>
-                            <span>
-                                Налог 5%
-                            </span>
-                            <div>
-
-                            </div>
-                            <b>
-                                {tax.toFixed(1)} руб.
-                            </b>
-                        </li>
-                    </ul>
-                    <button className='order-button'>Оформить заказ <img src="/img/arrow.svg" alt="arrow"/></button>
-                </div>
+                    <p className='text-center p'>Добавьте хотя бы одну пару <br/> кроссовок, чтобы сделать заказ.</p>
+                    <button onClick={onClose} className='back-button'><img src="/img/reverse-arrow.svg" className='reverse-arrow'
+                                                         alt="arrow"/> Вернуться
+                        назад
+                    </button>
+                </div>}
 
 
             </div>
